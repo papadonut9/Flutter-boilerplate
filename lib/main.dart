b801d11f54cda2201ab47670660756825dbf94be
papadonut9 <47145773+papadonut9@users.noreply.github.com>
2020-06-19 05:02:13 +0530

    minimalised the code
    Also added the list of files to be changed before proceeding

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Text('Hello World'),
+void main() => runApp(
+      MaterialApp(
+        home: null,
       ),
-    ),
-  );
-}
+    );
+
+//things to change after cloning this repo
+// 1. all 3 androidmanifest.xml
+// 2. gradle.build
+// 3. mainactivity.kt
+// 4. pubspec.yaml
