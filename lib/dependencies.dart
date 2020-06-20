24f0626db373645d05f07c6e1f08b455ade1a7bb
papadonut9 <47145773+papadonut9@users.noreply.github.com>
2020-06-20 16:34:15 +0530

    Create dependencies.dart

---
+++ lib/dependencies.dart
@@ -0,0 +1,6 @@
+//Flutter and Dart core requirements
+export 'package:flutter/material.dart';
+
+//local dependencies
+export 'package:flutter_boilerplate/main.dart';
+export 'package:flutter_boilerplate/internal/colors.dart';
